b3b3e82d6ae189124d9a0cfb3f9c139c81a86e4f
enisdenjo <denis@denelop.com>
2024-07-10 20:33:40 +0200

    more native

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,14 +1,9 @@
 import { createServeRuntime } from '@graphql-mesh/serve-runtime'
-import type { RequestEvent } from './$types';
 
 const serve = createServeRuntime({
   supergraph: 'schema.graphql', // working directory is root of the project
   graphqlEndpoint: '/api', // matches the server route in vite
+  fetchAPI: { Response } // use the native `Response`
 });
 
-async function handle(req: RequestEvent) {
-  const res = await serve(req)
-  return new Response(res.body, res) // convert mesh response to native response
-}
-
-export { handle as GET, handle as POST }
+export { serve as GET, serve as POST }
